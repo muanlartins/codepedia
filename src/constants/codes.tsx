c63aaa58b8672a7da9afe0a2f4927f40544b5b52
Luan Martins <muanlartins@gmail.com>
2024-10-06 19:33:41 -0300

    feat: adjust readme and deploy

--- src/constants/codes.tsx
+++ src/constants/codes.tsx
@@ -2,7 +2,8 @@ export const CODES = [
   {
     title: 'Greatest Common Divisor',
     tags: ['gcd'],
-    languages: ['cpp', 'typescript'],
+    languages: ['cpp'],
+    // languages: ['cpp', 'typescript'],
     codes: [
 `
 int gcd(int a, int b) {
@@ -11,41 +12,43 @@ int gcd(int a, int b) {
   return gcd(b, a%b);
 }
 `,
-`
-function gcd(a: number, b: number) {
-  if (!b) return a;
+// `
+// function gcd(a: number, b: number) {
+//   if (!b) return a;
 
-  return gcd(b, a%b);
-}
-`
+//   return gcd(b, a%b);
+// }
+// `
     ]
   },
   {
     title: 'Count occurrences of element',
     tags: ['count', 'occurrences'],
-    languages: ['cpp', 'typescript'],
+    languages: ['cpp'],
+    // languages: ['cpp', 'typescript'],
     codes: [
 `
 count(begin(a), end(a), x);
 `,
-`
-a.reduce((p, c) => p + 1, 0)
-`
+// `
+// a.reduce((p, c) => p + 1, 0)
+// `
     ]
   },
   {
     title: 'Count based on custom criteria',
     tags: ['count', 'custom'],
-    languages: ['cpp', 'typescript'],
+    languages: ['cpp'],
+    // languages: ['cpp', 'typescript'],
     codes: [
 `
 // e.g. Count even elements
 count_if(begin(a), end(a), [](int x) { return !(x%2); });
 `,
-`
-// e.g. Count even elements
-a.reduce((p, c) => p + !(c%2), 0)
-`
+// `
+// // e.g. Count even elements
+// a.reduce((p, c) => p + !(c%2), 0)
+// `
     ]
   }
 ];
